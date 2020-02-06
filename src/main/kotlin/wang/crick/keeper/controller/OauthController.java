1eefc7644ec2e7c4ced2fd8fca3b39ebc6509803
wangyuheng <wangyuheng@outlook.com>
2020-02-06 21:08:36 +0800

    set cookie for SSO

--- src/main/kotlin/wang/crick/keeper/controller/OauthController.java
+++ src/main/kotlin/wang/crick/keeper/controller/OauthController.java
@@ -17,8 +17,8 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.Serializable;
@@ -28,14 +28,13 @@ import java.util.*;
 @Controller
 public class OauthController {
 
-    Logger logger = LoggerFactory.getLogger(this.getClass());
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Value("${oauth2.server.url:https://gitlab.com}")
     private String gitlabServerUrl;
-
-    @Value("${oauth2.client.id:5e6ee9a97d02281ae979af4b714eabf246b256c631114cc4478e67d3d77bbbb0}")
+    @Value("${oauth2.client.id:2f7e660699a37776a91fcccc855b6f3e007914bc34335ca7a0fc77233e818643}")
     private String clientId;
-    @Value("${oauth2.client.secret:737898bcc0cdead654a0b94cdc1d4084a6309d7ec708da25bfda2734df72e5b9}")
+    @Value("${oauth2.client.secret:324012c49eaaa60a6dc1147f166427757b4502968b94e07d3da1cbc8f931deec}")
     private String clientSecret;
     @Value("${oauth2.client.callback.url:http://localhost:9000/callback}")
     private String callbackUrl;
@@ -45,36 +44,44 @@ public class OauthController {
     private Map<String, User> userStore = new HashMap<>();
     private RestTemplate restTemplate = new RestTemplate();
 
-    @GetMapping("/main")
+    @GetMapping({"/main","/"})
     @ResponseBody
-    public String main(HttpServletRequest req, HttpServletResponse response) {
+    public String main() {
         User user = (User) RequestContextHolder.getRequestAttributes().getAttribute(CURRENT_USER, RequestAttributes.SCOPE_SESSION);
-        return "<html><body>hi:" + user.username + "</body></html>";
+        return "<html><body>hi:" + user.username + " This is Main</body></html>";
     }
 
-
+    /**
+     * 授权后redirect url
+     * @param code 用于获取accessToken，只能使用一次
+     */
     @GetMapping("/callback")
     public String callback(@RequestParam(value = "code", required = false) String code,
-                           RedirectAttributes redirectAttributes,
-                           HttpServletRequest request) {
+                           HttpServletRequest request, HttpServletResponse response) {
         String referer = request.getParameter("referer");
         String accessToken = getAccessToken(code, buildCallbackUrl(referer));
         User user = getUser(accessToken);
 
         String uuid = UUID.randomUUID().toString();
         userStore.put(uuid, user);
-        redirectAttributes.addAttribute(AUTHORIZATION_KEY, uuid);
+        //set cookie
+        response.addCookie(new Cookie(AUTHORIZATION_KEY, uuid));
         return "redirect:" + referer;
-    }
-
-    private User getUser(String accessToken) {
-        return restTemplate.getForObject(gitlabServerUrl + "/api/v4/user?access_token=" + accessToken, User.class);
     }
 
     private String buildCallbackUrl(String referer) {
         return callbackUrl + "?referer=" + referer;
     }
 
+    private User getUser(String accessToken) {
+        return restTemplate.getForObject(gitlabServerUrl + "/api/v4/user?access_token=" + accessToken, User.class);
+    }
+
+    /**
+     * 通过code去gitlab获取accessToken
+     * @param code grant ticket 只能使用一次
+     * @param redirectUri 回调地址，必须与授权时参数一致
+     */
     private String getAccessToken(String code, String redirectUri) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
@@ -104,8 +111,16 @@ public class OauthController {
                     new HandlerInterceptorAdapter() {
                         @Override
                         public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-                            String authorization = Optional.ofNullable(request.getParameter(AUTHORIZATION_KEY)).orElse(request.getHeader(AUTHORIZATION_KEY));
-                            if (authorization == null) {
+                            Optional<String> authorizationKeyOp = Arrays.stream(request.getCookies())
+                                    .filter(it->it.getName().equals(AUTHORIZATION_KEY))
+                                    .map(Cookie::getValue)
+                                    .findAny();
+                            if (authorizationKeyOp.isPresent()) {
+                                // 授权信息存在，获取user信息放入session
+                                RequestContextHolder.getRequestAttributes().setAttribute(CURRENT_USER, userStore.get(authorizationKeyOp.get()), RequestAttributes.SCOPE_SESSION);
+                                return super.preHandle(request, response, handler);
+                            } else {
+                                // 授权信息不存在，去gitlab进行验证
                                 String referer = request.getRequestURL().toString();
                                 String redirectUri = URLEncoder.encode(buildCallbackUrl(referer), "utf-8");
                                 String gitlabAuthUrl = gitlabServerUrl + "/oauth/authorize?response_type=code&redirect_uri=" + redirectUri + "&client_id=" + clientId;
@@ -113,9 +128,6 @@ public class OauthController {
                                 response.sendRedirect(gitlabAuthUrl);
                                 return false;
                             }
-                            response.addHeader("Authorization", authorization);
-                            RequestContextHolder.getRequestAttributes().setAttribute(CURRENT_USER, userStore.get(authorization), RequestAttributes.SCOPE_SESSION);
-                            return super.preHandle(request, response, handler);
                         }
                     })
                     .addPathPatterns("/main", "/test");
